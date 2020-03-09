75d151a3f9d5bcb8800fd728d17261b78c9f98d2
Maximilien Mellen <maxmellen0@gmail.com>
2020-03-09 07:54:06 +0100

    Under construction...

--- src/Main.elm
+++ src/Main.elm
@@ -1,57 +1,11 @@
-port module Main exposing (main)
+module Main exposing (main)
 
-import Browser
 import Html as H exposing (Html)
 import Html.Attributes as A
-import Html.Events as E
 import Styles
 
 
-type alias Flags =
-    ()
-
-
-type alias Model =
-    ()
-
-
-type Msg
-    = NoOp
-    | Alert String
-
-
-main : Program Flags Model Msg
+main : Html msg
 main =
-    Browser.element
-        { init = init
-        , view = view
-        , update = update
-        , subscriptions = \_ -> Sub.none
-        }
-
-
-init : Flags -> ( Model, Cmd Msg )
-init _ =
-    ( (), Cmd.none )
-
-
-view : Model -> Html Msg
-view _ =
-    H.p [ A.class Styles.big, E.onClick <| Alert "I said hey!" ]
-        [ H.text "And welcome to "
-        , H.span [ A.class Styles.elm ] [ H.text "Elm" ]
-        , H.text "!"
-        ]
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        NoOp ->
-            ( model, Cmd.none )
-
-        Alert message ->
-            ( model, alert message )
-
-
-port alert : String -> Cmd msg
+    H.p [ A.class Styles.big ]
+        [ H.text "Under construction..." ]
--- src/main.ts
+++ src/main.ts
@@ -1,55 +1,6 @@
-import styles from "./styles.scss";
+import "./styles.scss";
 import { Elm } from "./Main.elm";
 
-interface Greetings {
-  phrase: string;
-  technology: string;
-  color: string;
-}
-
-let greetings: Greetings[] = [
-  {
-    phrase: "Hello",
-    technology: "Parcel",
-    color: styles.parcel
-  },
-  {
-    phrase: "Oh hi",
-    technology: "TypeScript",
-    color: styles.typescript
-  },
-  {
-    phrase: "Helllooo",
-    technology: "CSS modules",
-    color: styles.cssModules
-  }
-];
-
-let punctuation: string[] = [",", "...", "!"];
-
-let paragraphs: HTMLParagraphElement[] = greetings.map((g, i) => {
-  let p = document.createElement("p");
-  let span = document.createElement("span");
-  span.textContent = g.technology;
-  span.className = g.color;
-  p.append(`${g.phrase}, `);
-  p.append(span);
-  p.append(punctuation[i]);
-  p.className = styles.big;
-  return p;
-});
-
-window.addEventListener("load", () => {
-  paragraphs.forEach(p => {
-    document.body.append(p);
-  });
-
-  const elmDiv = document.createElement("div");
-  document.body.append(elmDiv);
-
-  let app = Elm.Main!.init({ node: elmDiv });
-
-  app.ports!.alert!.subscribe!((message: string) => {
-    window.alert(message);
-  });
+Elm.Main!.init({
+  node: document.getElementById("elm")!
 });
